6b5371da096eeab7e366b0cf64bf17911e39fd7d
Jorge Contreras <jorge.contreras@cin7.com>
2021-04-18 20:25:53 +1200

    API DB Context Setup

---
+++ Reactivities/Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Generates a Table call Activities for our Activity Object
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
